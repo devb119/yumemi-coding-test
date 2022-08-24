b3c5c605fac63451fece38c3d148eeac908cc2b4
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-08-24 10:53:56 +0700

    config prettier + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': 0,
+    'import/prefer-default-export': 'off',
+    'import/no-self-import': 'off',
+    'no-underscore-dangle': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'import/no-named-as-default': 0,
+    'jsx-a11y/alt-text': [0],
+  },
+};
