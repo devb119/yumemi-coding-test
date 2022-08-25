bd8945e348a78f5e4ef6e3c0baea3ba8f253597e
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-08-25 11:28:11 +0700

    add chart to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Footer from './components/Layout/Footer';
 import Header from './components/Layout/Header';
+import Chart from './components/Chart';
 
 const App = () => (
   <>
     <Header />
-    <div className="container"> Content</div>
+    <div className="container">
+      <Chart />
+    </div>
     <Footer />
   </>
 );
